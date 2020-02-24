dc220fe09b6c43d0d2da039fbc303bc9029e204f
Ansonator <ansonator@hotmail.com>
2020-02-24 00:05:17 -0600

    experimentation

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-# python3 -m pip install --user -r requirements.txt
-python3 -m pip install --user pipenv
-pipenv install --ignore-pipfile
+python3 -m pip install --user -r requirements.txt
+#python3 -m pip install --user pipenv
+#pipenv install --ignore-pipfile
--- start.sh
+++ start.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 export PYTHONUNBUFFERED=true
-pipenv run python server.py
+#pipenv run python server.py
+python3 server.py
